7400ab5f4997db9a3eab7734945e616a2fff83be
Mdarish120 <arishmd768@gmail.com>
2022-03-01 12:44:49 +0530

    added comment

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json({limit:"30mb" ,extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb" ,extended:true}));
 app.use(cors());
 
-
+//api routes
 app.use('/api',router);
 
 app.get('/',(req,res)=>{
